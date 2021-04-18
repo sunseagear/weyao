c2025a83562b0db0ca50e6cfcb068c79d211b0d2
liangwei <dothegod@gmail.com>
2021-04-18 22:21:31 +0800

    增加新闻样例模块

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,7 +3,7 @@
     <nav-bar :left-arrow="false" />
     <van-swipe :autoplay="3000" style="height: 300px">
       <van-swipe-item v-for="(image, index) in images" :key="index">
-        <van-image :src="image" fit="fill" />
+        <van-image :src="image" fit="fill" height="300"/>
       </van-swipe-item>
     </van-swipe>
     <van-grid>
@@ -12,16 +12,19 @@
       <van-grid-item icon="photo-o" text="文字" />
       <van-grid-item icon="photo-o" text="文字" />
     </van-grid>
+    <van-cell :icon="require('../assets/icon/fillet.png')" title="新闻资讯" value="更多" class="newsDetails" @click="newsDetails" />
     <van-cell-group>
       <van-cell v-for="(item, index) in list.slice(0, 5)" :key="index" @click="toNews(item.id)">
-        <img v-if="item.img" :src="item.img" alt="" class="NewsImg" >
-        <div :class="item.img ? 'newsList' : 'noImgNewsList'">
-          <div>
-            <h3>{{ item.newsContentTitle }}</h3>
-            <p>{{ item.newsTypeName }}</p>
+        <div style="display: flex;">
+          <img v-if="item.img" :src="item.img" alt="" class="NewsImg" >
+          <div :class="item.img ? 'newsList' : 'noImgNewsList'">
+            <div>
+              <h3>{{ item.newsContentTitle }}</h3>
+              <p>{{ item.newsTypeName }}</p>
+            </div>
+            <span>{{ item.newsReleaseTime | parseTime('{y}-{m}-{d}') }}</span>
+            <span class="seePeople">{{ item.readingsNumber }}人阅读</span>
           </div>
-          <span>{{ item.newsReleaseTime | parseTime('{y}-{m}-{d}') }}</span>
-          <span class="seePeople">{{ item.readingsNumber }}人阅读</span>
         </div>
       </van-cell>
     </van-cell-group>
@@ -37,8 +40,8 @@ export default {
     return {
       list: [],
       images: [
-        'https://img.yzcdn.cn/vant/apple-1.jpg',
-        'https://img.yzcdn.cn/vant/apple-2.jpg'
+        'http://5b0988e595225.cdn.sohucs.com/images/20191201/73c1e7a9573d41a59bd72bc9886d9030.jpeg',
+        'http://5b0988e595225.cdn.sohucs.com/images/20191201/80db4da6585444bfa49ccab8e5628f3e.jpeg'
       ]
     }
   },
@@ -50,6 +53,9 @@ export default {
       newsPageContent().then(res => {
         this.list = res.data.data
       })
+    },
+    newsDetails() {
+      this.$router.push('/newsList')
     },
     toNews(id) {
       this.$router.push({
@@ -80,7 +86,7 @@ export default {
   box-sizing: border-box;
   padding-left: 5px;
   float: right;
-  width: 220px;
+  width: 100%;
   margin: 0 auto;
 //padding-bottom: 10px;
 }
@@ -90,10 +96,6 @@ export default {
 .newsList h3 {
   margin: 0;
 }
-h3 {
-  font-size: 16px;
-  font-weight: normal;
-}
 .NewsImg {
   float: left;
   width: 115px;
@@ -101,5 +103,7 @@ h3 {
   border-radius: 5px;
   margin-top: 4px;
 }
-
+.seePeople {
+  float: right;
+}
 </style>
